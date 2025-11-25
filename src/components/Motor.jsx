394d5226fce3d06298a88921eaf561d9b96207ae
Alexander <test@tests-MacBook-Air.local>
2025-11-25 17:39:02 +0200

    Motor section responsive

--- src/components/Motor.jsx
+++ src/components/Motor.jsx
@@ -5,20 +5,20 @@ import React from 'react'
 const Motor = () => {
     return (
         <div className='w-full pt-5 bg-[#0C0D0D] text-white'>
-            <div className='mx-auto max-w-[1440px] px-5 gap-x-20  flex'>
+            <div className='mx-auto max-w-[1440px] px-5 gap-x-20  flex flex-col motor-md:flex-row'>
                 {/* left */}
                 <div className='flex-[30%]'>
                     <h3 className='mb-4 text-[20px] font-medium text-center specific-xl:text-start specific-xl:mb-0'>MOTOR A BATÉRIA</h3>
                 </div>
 
                 {/* right */}
-                <div className='flex-[70%]'>
+                <div className='flex-[70%] flex flex-col items-center motor-md:block'>
                     <h3 className='text-[20px] font-medium'>SHIMANO STEPS EP8</h3>
                     
                     <img src="/motor/img1.png" alt="main-img" className='mt-[30px] mb-[43px] object-cover' />
 
-                    <div className='flex justify-between gap-[45px] pb-28'>
-                        <div className='flex flex-col max-w-[430px] space-y-5'>
+                    <div className='flex flex-col motor-xl:flex-row justify-between gap-10 motor-md:gap-[7px] motor-xl:gap-[45px] pb-28'>
+                        <div className='flex flex-col motor-md:max-w-[430px] space-y-5'>
                             <img src="/motor/img2.png" alt="icon-lft" className='w-10 h-10' />
                             
                             <h4 className='font-medium'>250W motor STEPS EP8</h4>
@@ -30,7 +30,7 @@ const Motor = () => {
                             </div>
                         </div>
 
-                        <div className='flex flex-col max-w-[430px] space-y-5'>
+                        <div className='flex flex-col motor-md:max-w-[430px] space-y-5'>
                             <img src="/motor/img3.png" alt="icon-right" className='w-10 h-10' />
                             
                             <h4 className='font-medium'>630 Wh batéria Shimano BT-E8036</h4>
